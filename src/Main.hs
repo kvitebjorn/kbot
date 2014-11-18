6fe9215a81a2df687b32533d6641b81af81034f2
kvitebjorn <kvitebjorn.k@gmail.com>
2014-11-18 00:56:34 -0700

    connecting to server and channel

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,45 @@
 module Main where
 
+import Control.Monad ( forever )
+import Data.List ( isInfixOf )
+import Network
+import System.IO 
+
+host :: HostName
+host = "irc.freenode.org"
+
+port :: PortNumber
+port = 6667
+
+nick :: String
+nick = "kvitebot"
+
+channel :: String
+channel = "##norsk"
+
 main :: IO ()
-main = putStrLn "initial"
+main = withSocketsDo $ do
+  putStrLn $ "Connecting to " ++ host ++ " on port " ++ show port
+
+  -- Connect to freenode
+  h <- connectTo host $ PortNumber port
+  
+  -- User authentication
+  hPutStrLn h $ "USER " ++ nick ++ " " ++ nick ++ " " ++ nick ++ " :This is kbot"
+
+  -- Set nick
+  hPutStrLn h $ "NICK " ++ nick
+
+  -- TODO: /msg nickserv for identify on nick
+
+  -- Join channel
+  hPutStrLn h $ "JOIN " ++ channel
+
+  -- Receive 
+  forever $ do
+    -- Receive msg
+    msg <- hGetLine h
+    putStrLn msg
+
+    -- TODO: Check for pings from server
+
