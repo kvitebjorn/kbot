0c94d4163e548b70192f9bbe272214065854b1cd
kvitebjorn <kvitebjorn.k@gmail.com>
2014-11-23 16:48:04 -0700

    added null pattern match

--- src/Dict/Lookup.hs
+++ src/Dict/Lookup.hs
@@ -7,6 +7,7 @@ import Data.List ( isPrefixOf )
 import Dict.Entries
 
 lookupEntry :: String -> IO [String]
+lookupEntry ""  = return []
 lookupEntry str = case toLower $ head str of
   'a' -> filter (str `isPrefixOf`) <$> a
   'b' -> filter (str `isPrefixOf`) <$> b
