e8e40c22a7b0bd027f48644dd06f8f355211464c
kvitebjorn <kvitebjorn.k@gmail.com>
2014-11-23 20:21:34 -0700

    refactoring

---
+++ src/IRC/Connection.hs
@@ -0,0 +1,16 @@
+module IRC.Connection where
+
+import Network
+
+host :: HostName
+host = "irc.freenode.org"
+
+port :: PortNumber
+port = 6667
+
+nick :: String
+nick = "kvitebot"
+
+channel :: String
+channel = "##kvitebottest"
+
